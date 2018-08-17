922e515cbc7fbd08ec19d8fe952d265d23328929
Naatoo <thenatooorat@gmail.com>
2018-08-17 19:40:27 +0200

    Change connection to db. Add queries

---
+++ dbtools.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from base import Base
+import os
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class DbTool(metaclass=Singleton):
+
+    def __init__(self):
+        db_name = 'game.db'
+        if os.path.isfile(db_name):
+            os.remove(db_name)
+        self.engine = create_engine('sqlite:///{}'.format(db_name), echo=False)
+        self.session = Session(self.engine)
+        Base.metadata.create_all(self.engine)
+
+    def get_all_rows(self, table_name, first_to_eq=None, second_to_eq=None):
+        return self.session.query(table_name).filter(first_to_eq == second_to_eq).all() if first_to_eq is not None \
+            else self.session.query(table_name)
+
+    def get_one_row(self, table_name, first_to_eq, second_to_eq):
+        return self.session.query(table_name).filter(first_to_eq == second_to_eq).one()
+
+    def get_one_column(self, column_name):
+        return self.session.query(column_name)
+
+    def insert_row(self, row):
+        self.session.add(row)
+
+    def insert_many_rows(self, rows: list):
+        for row in rows:
+            self.session.add(row)
+
+    def finish(self):
+        self.session.commit()
+        self.session.close()
