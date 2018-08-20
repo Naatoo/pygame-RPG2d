a24a85494a90e171459c2f41400d9bc3852333b1
Naatoo <thenatooorat@gmail.com>
2018-08-20 21:08:55 +0200

    Format output using decorator

--- game/output/display_formatter.py
+++ game/output/display_formatter.py
@@ -1,11 +1,18 @@
-def build_message(message):
-    print_message(standard_format("+").format(message))
-
-
-def standard_format(sign):
-    deco = "{}".format(sign) * 50
-    return "\n".join([deco, "{}", deco])
-
-
 def print_message(message):
-    print(message)
+    print(build_message(message))
+
+
+def formatter(sign="-", quantity=50):
+    def decorate(func):
+        def formatted(*args):
+            result = func(*args)
+            prepare_places = [item * quantity if len(item) < 3 else item for item in result]
+            final_message = "\n".join(prepare_places).format(*(sign for _ in range(quantity * 2)))
+            return final_message
+        return formatted
+    return decorate
+
+
+@formatter(sign="+", quantity=25)
+def build_message(message):
+    return ["{}", message, "{}"]
--- game/output/messages.py
+++ game/output/messages.py
@@ -1,7 +1,8 @@
-from game.output.display_formatter import build_message
+from game.output.display_formatter import print_message
 
 
 def forbidden_field_to_move(direction):
     cardinal_directions = dict(zip(("w", "s", "a", "d"), ("north", "south", "west", "east")))
     message = "You cannot go {}.".format(cardinal_directions[direction])
-    build_message(message)
+    print_message(message)
+
