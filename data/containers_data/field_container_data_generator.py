c1ca33a681344dd34186b3e815cfdb6b7df75b35
Naatoo <thenatooorat@gmail.com>
2018-09-15 00:57:54 +0200

    Split map data json to separate Field and FieldType files

--- data/containers_data/field_container_data_generator.py
+++ data/containers_data/field_container_data_generator.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 
 
 with open(FIELD_CONTAINERS_DATA_FILE, "a") as data:
-    row_data = [(OrderedDict(id_container=field_id, container_type_id=2, container_field_id=field_id)) for field_id in range(1, 401)]
+    row_data = [(OrderedDict(id_container=field_id, container_type_id=2, container_field_id=field_id))
+                for field_id in range(1, 401)]
     table = {"Container": row_data}
     data.write(json.dumps(table))
