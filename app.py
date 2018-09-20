a20b3f134eec37ac3b296e6da7f0545e9ce121c8
Naatoo <thenatooorat@gmail.com>
2018-09-20 22:29:16 +0200

    Rename redundant files - formatters and printers in command line

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from src.database.db_tool import DbTool
 from src.database.initial_data_insertion import load_initial_data
-from src.objects.world_map import WorldMap
-from src.operations.input_checker import ActionChecker
 from src.game.main import game_loop
 
 try:
     dbtool = DbTool()
     load_initial_data()
-    world_map = WorldMap()
-    action_checker = ActionChecker()
     game_loop()
 
 finally:
