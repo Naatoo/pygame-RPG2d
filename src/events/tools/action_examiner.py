9ef2ef7b70db5ab74b2d55916bd879e7a088c91a
Naatoo <thenatooorat@gmail.com>
2018-09-29 22:40:37 +0200

    Fix item collecting. Remove ActionInvoker class - leave plain functions.

--- src/events/tools/action_examiner.py
+++ src/events/tools/action_examiner.py
@@ -1,4 +1,5 @@
 from src.database.db_tool import DbTool
+from src.events.tools.db_updates import move_by_mouse, move_by_keys, collect_item
 
 
 class ActionExaminer:
@@ -12,26 +13,25 @@ class ActionExaminer:
 
     def check_move_by_mouse(self, position: tuple, lmb_click_down_position: tuple):
         if position == lmb_click_down_position:  # TODO and when field is accessible
-            return True
+            move_by_mouse(position)
         else:
             return False
 
     def check_move_by_key(self, key):
         if True:  # TODO check if field is accessible
-            return True
+            move_by_keys(key)
         else:
             return None
 
     def check_collect_item(self, position: tuple):
-        if self.item_in_field(position) is not None:
-            return True
+        item = self.item_in_field(position)
+        if item is not None:
+            collect_item(position)
         else:
             return False
 
     @staticmethod
     def item_in_field(position):
         player = DbTool().get_player
-        print(player.x - 7 + position[0] // 64)
         return DbTool().get_element_in_coordinates(('src.objects.items', 'BoundedItem'), player.x - 7 + position[0] // 64,
                                                    player.y - 7 + position[1] //64)
-
--- src/events/tools/event_handler.py
+++ src/events/tools/event_handler.py
@@ -3,7 +3,6 @@ import pygame
 from src.events.input_collectors.keyboard_input import KeyboardInput
 from src.events.input_collectors.mouse_input import MouseInput
 from src.events.tools.check_input import CheckInput
-from src.events.tools.action_invoker import ActionInvoker
 from src.events.tools.display import Display
 
 
@@ -14,12 +13,9 @@ class EventHandler:
         self.keyboard_input = KeyboardInput()
         self.mouse_input = MouseInput()
         self.input_checker = CheckInput(self.mouse_input, self.keyboard_input)
-        self.action_invoker = ActionInvoker()
         self.display = Display()
 
     def handle_event(self, event: pygame.event):
-        action = None
-        event_data = None
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.mouse_input.add_element(button_id=event.button, position=event.dict['pos'])
         elif event.type == pygame.KEYDOWN:
@@ -32,12 +28,12 @@ class EventHandler:
         self.input_checker.update_inputs(self.mouse_input, self.keyboard_input)
         if event.type == pygame.MOUSEBUTTONUP:
             event_data = dict(button_id=event.button, position=event.dict['pos'])
-            action = self.input_checker.check_call_mouse_button_up(**event_data)
+            self.input_checker.check_call_mouse_button_up(**event_data)
             self.mouse_input.clear()
 
         elif event.type == pygame.KEYDOWN:
             event_data = dict(key=event.key)
-            action = self.input_checker.check_call_key_down(**event_data)
+            self.input_checker.check_call_key_down(**event_data)
 
         elif event.type == pygame.KEYUP:
             self.input_checker.check_call_key_up(key=event.key)
@@ -52,6 +48,3 @@ class EventHandler:
             pass
             print("Unknown event")
         # TODO send camera position instead of click position
-        if action is not None:
-            fn = getattr(self.action_invoker, action)
-            fn(**event_data)
